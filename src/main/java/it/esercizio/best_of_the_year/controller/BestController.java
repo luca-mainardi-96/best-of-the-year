7d4feeb5895063f40291b7e9eddb0c42c57d59e9
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-10-04 19:11:37 +0200

    non funziona niente

--- src/main/java/it/esercizio/best_of_the_year/controller/BestController.java
+++ src/main/java/it/esercizio/best_of_the_year/controller/BestController.java
@@ -1,5 +1,53 @@
 package it.esercizio.best_of_the_year.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import it.esercizio.best_of_the_year.model.Movie;
+import it.esercizio.best_of_the_year.model.Song;
+
+
+@Controller
+
 public class BestController {
 
+    @GetMapping("/")
+    public String home(Model model) {
+       String authorName = "Luca Mainardi";
+       model.addAttribute("authorName", authorName);
+       return "index";  
+    }
+
+    private List<Movie> getBestMovies(){
+        List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie(1, "A"));
+        movies.add(new Movie(2, "B"));
+        movies.add(new Movie(3, "C"));
+        return movies;
+    }
+
+   private List<Song> getBestSongs(){
+        List<Song> songs = new ArrayList<>();
+        songs.add(new Song(1, "X"));
+        songs.add(new Song(2, "Y"));
+        songs.add(new Song(3, "Z"));
+        return songs;
+   }
+
+    @GetMapping("/movies")
+    public String getMovies(Model model) {
+        model.addAttribute("movies", getBestMovies());
+        return "index";
+    }
+
+    @GetMapping("/songs")
+    public String getSongs(Model model) {
+        model.addAttribute("songs", getBestSongs());
+        return "index";
+    }
+   
 }
--- src/main/java/it/esercizio/best_of_the_year/model/Movie.java
+++ src/main/java/it/esercizio/best_of_the_year/model/Movie.java
@@ -2,4 +2,29 @@ package it.esercizio.best_of_the_year.model;
 
 public class Movie {
 
+    private int id;
+    private String title;
+
+    public Movie(int id, String title){
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
 }
